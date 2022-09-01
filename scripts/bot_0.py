e9d6ffe74f53afb5f266888679dd5083fc498fad
hyi1014 <hyi28915088@gmail.com>
2022-09-01 16:49:19 +0800

    feat: add the reply function of robot which cab bibibi

--- scripts/bot_0.py
+++ scripts/bot_0.py
@@ -1,4 +1,21 @@
+import os
 import discord
-from discord.ext import commands
+from dotenv import load_dotenv
 
-bot = commands.Bot(command_prefix='$')
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'
+          .format(client))
+    
+@client.event
+async def on_message(cxt):
+    if cxt.author == client.user:
+        return
+    
+    if cxt.content.startswith('$'):
+        await cxt.channel.send('bibibi')
+        
+load_dotenv()
+client.run(os.getenv("DC_TOKEN", default=""))
